6be4e2ac848e2cbecf81e2641f2b215b03d6c4e1
RealDrive <dimaeve@gmail.com>
2023-08-21 17:37:36 +1000

    a2

--- core/psql.py
+++ core/psql.py
@@ -1,16 +1,15 @@
 import asyncpg
 
 
-async def on_startup_psql(dp):
-    db_pool = await asyncpg.create_pool(
-        host='37.220.83.23',
-        port=5432,
-        database='financebot',
-        user='postgres',
-        password='Flatron1984',
-    )
+HOST = '37.220.83.23'
+PORT = 5432
+DB = 'financebot'
+USER = 'postgres'
+PSWD = 'Flatron1984'
 
-    dp['db'] = db_pool
+
+async def check_user_exists(user_id):
+    conn = await asyncpg.connection(user=USER, password=PSWD, da)
 
 
 async def check_user_exists(connection, user_id):
@@ -22,3 +21,4 @@ async def check_user_exists(connection, user_id):
 async def register_user(connection, user_id, username):
     query = "INSERT INTO users (user_id, username, balance) VALUES ($1, $2, 0.0)"
     await connection.execute(query, user_id, username)
+
--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message, ContentType
+from aiogram.types import Message
 import asyncio
 import logging
 from core.settings import settings
 from core.utils.commands import set_commands
-from core.psql import check_user_exists, register_user
-
+from core.psql import check_user_exists, on_startup_psql, register_user
+from aiogram.filters import Command
 
 
 async def start_bot(bot: Bot):
     await set_commands(bot)
-    await register_user(bot)
     await bot.send_message(settings.bots.admin_id, text='Бот запущен')
+
+
+async def req_user(message: Message):
+    user_id = message.from_user.id
+    async with dp['db'].acquire() as connection:
+        user_exists = await check_user_exists(connection, user_id)
+
+        if not user_exists:
+            await register_user(connection, user_id, message.from_user.username)
+            await message.answer('Вы успешно зарегистрированы')
+
+        else:
+            await message.answer('Вы уже зарегестрированы')
 
 
 async def stop_bot(bot: Bot):
@@ -26,8 +38,11 @@ async def start():
 
     dp = Dispatcher()
 
+    await on_startup_psql(dp)
+    dp.register_message_handler(req_user, Command(commands='requser'))
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
+
 
     try:
         await dp.start_polling(bot)
